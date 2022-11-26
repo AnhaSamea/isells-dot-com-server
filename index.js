7f127f8ab85332e4786894127fa09b1311beb798
anhasamea <anhasamea707@gmail.com>
2022-11-26 08:57:48 +0600

    database connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -7,6 +8,11 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w75vwv7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/',async(req,res)=>{
     res.send('iSells.com server is running');
